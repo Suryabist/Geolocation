b9212599a186c34c062f3c23ec25f1962daca0ec
Surya Bista <sbistaiw@gmail.com>
2019-12-23 15:27:35 +0545

    added the seprate functionality for render

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './SeasonDisplay.css'
+
+const seasonConfig = {
+    Summer : { 
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    Winter : { 
+        text: "Burr it is cold",
+        iconName: 'snowflake'
+
+    }
+}
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9 ) { 
-       return lat > 0 ? 'summer' : 'Winter'
+       return lat > 0 ? 'Summer' : 'Winter'
     } else{
        return lat > 0 ?  'Winter' : 'Summer'
     }
@@ -10,11 +23,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDispaly = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'Winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+    const {text, iconName} = seasonConfig[season]
+    
     return(
-        <div>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
            {text}
+           <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 }
 export default SeasonDispaly
+ 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import SeasonDispaly from './SeasonDisplay'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDispaly from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
   state = {lat: null, errorMessage: ''}
-    componentDidMount(){
 
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({lat: position.coords.latitude}),      
             err =>  this.setState({errorMessage: err.message})
         );
-    }
-
-    // Most render the react during 
-    render(){     
-     
-            if(this.state.errorMessage && !this.state.lat){
-                return <div>Error:{this.state.errorMessage}</div>
-            }
-
-            if (this.state.lat && !this.state.errorMessage) {
-                return <SeasonDispaly  lat={this.state.lat}/>
-            }
-                return <div>Loading...</div>
-            }
-            
     };
 
+    renderContetn() {
+
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error:{this.state.errorMessage}</div>
+        }
+
+        if (this.state.lat && !this.state.errorMessage) {
+            return <SeasonDispaly  lat={this.state.lat}/>
+        }
+            return <Spinner message="Please allow us for location"/>
+        }
+
+    render() {
+        return(
+
+            <div className="border red">
+                {this.renderContetn()}
+            </div>
+        );
+    }
+};
+  
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
